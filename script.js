f65791141aa5e4a56fcc3c87fe63cb6d4ab474ed
Kalen Buchanan <Kalenbuchanan24@gmail.com>
2023-07-15 11:00:38 -0600

    completed some of it some more touch ups are needed.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const modeToggleCheckbox = document.getElementById('mode-toggle-checkbox');
-const body = document.body;
+const navbar = document.querySelector('nav');
+const navbarLinks = document.querySelectorAll('.nav-links li a');
 
 modeToggleCheckbox.addEventListener('change', toggleDarkLightMode);
 
 function toggleDarkLightMode() {
   if (modeToggleCheckbox.checked) {
-    body.classList.add('dark-mode');
+    document.body.classList.add('dark-mode');
+    navbar.style.backgroundColor = '#333';
+    navbarLinks.forEach(link => {
+      link.style.color = '#fff';
+    });
   } else {
-    body.classList.remove('dark-mode');
+    document.body.classList.remove('dark-mode');
+    navbar.style.backgroundColor = '#f2f2f2';
+    navbarLinks.forEach(link => {
+      link.style.color = '#333';
+    });
   }
 }
